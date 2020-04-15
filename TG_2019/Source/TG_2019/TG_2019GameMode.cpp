6f81c7b6084a0cf2e479a68d7dd40f0514bc0a99
MinhBo2 <minhbo0000@gmail.com>
2020-04-15 15:30:04 -0700

    dynamic navmesh and level map

--- TG_2019/Source/TG_2019/TG_2019GameMode.cpp
+++ TG_2019/Source/TG_2019/TG_2019GameMode.cpp
@@ -9,7 +9,7 @@ ATG_2019GameMode::ATG_2019GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("Game/Dynamic/Character/Behaviors/Character_BP"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviors/Character_BP"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
